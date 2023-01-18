6db2723f5284f6e475c5861e8300a0eddd0aabc4
Nasarsoft <nasarsoft@gmail.com>
2023-01-18 20:25:43 +0530

    catogory ,product, insertion API added

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getCategories, newCategory} = require("../controllers/categoryController")
+const {getCategories, newCategory, deleteCategory} = require("../controllers/categoryController")
 
 router.get("/", getCategories)
 router.post("/", newCategory)
+router.delete("/:category", deleteCategory)
 
 module.exports = router
